122031e473bff3d528b4253b38671e8fd4342d3c
cracrayol <cracrayol@free.fr>
2024-04-03 22:25:25 +0200

    Fix tests

--- test/suite/extension.test.ts
+++ test/suite/extension.test.ts
@@ -16,12 +16,14 @@ import { Config } from '../../src/lib/config';
 const PMD_PATH = path.join(__dirname, '..', '..', '..', 'bin', 'pmd');
 const RULESET_PATH = path.join(__dirname, '..', '..', '..', 'rulesets', 'quickstart.xml');
 const INVALID_RULESET_PATH = path.join(__dirname, '..', '..', '..', 'rulesets', 'java_ruleset_invalid.xml');
-const TEST_JAVA_PATH = path.join(__dirname, '..', '..', '..', 'test', 'assets', 'test.java');
+const TEST_ASSETS_PATH = path.join(__dirname, '..', '..', '..', 'test', 'assets');
+const TEST_JAVA_PATH = path.join(TEST_ASSETS_PATH, 'test.java');
+
+const outputChannel = vscode.window.createOutputChannel('Java PMD');
+
 
 suite('Extension Tests', () => {
   test('check default paths', () => {
-    const outputChannel = vscode.window.createOutputChannel('Java PMD');
-
     const config = new Config();
     config.pmdBinPath = PMD_PATH;
     config.rulesets = [RULESET_PATH, INVALID_RULESET_PATH];
@@ -31,18 +33,17 @@ suite('Extension Tests', () => {
 
     const pmd = new JavaPmd(outputChannel, config);
 
-    assert.strictEqual(pmd.checkPmdPath(), true);
-    assert.strictEqual(pmd.getRulesets()[0], RULESET_PATH);
-    assert.strictEqual(pmd.getRulesets().length, 1);
-    assert.strictEqual(pmd.hasAtLeastOneValidRuleset(), true);
+    assert.equal(pmd.checkPmdPath(), true);
+    assert.equal(pmd.getRulesets()[0], RULESET_PATH);
+    assert.equal(pmd.getRulesets().length, 1);
+    assert.equal(pmd.hasAtLeastOneValidRuleset(), true);
   });
 
-  // Defines a Mocha unit test
+// Defines a Mocha unit test
   test('test diagnostic warning', function (done) {
     this.timeout(100000);
 
     const collection = vscode.languages.createDiagnosticCollection('java-pmd-test');
-    const outputChannel = vscode.window.createOutputChannel('Java PMD');
 
     const config = new Config();
     config.pmdBinPath = PMD_PATH;
@@ -64,8 +65,7 @@ suite('Extension Tests', () => {
         done();
       })
       .catch((e) => {
-        assert.fail(e);
-        done();
+        done(e);
       });
   });
 });
